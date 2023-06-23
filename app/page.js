e302fa20ca9f431043f8a8c951c04659cf3db2a9
Kitsune <87698261+Kat3110@users.noreply.github.com>
2023-06-23 16:18:37 +0600

    adding animations on the home page fixing slider and page 404

--- app/page.js
+++ app/page.js
@@ -1,17 +1,20 @@
-import Link from 'next/link';
+"use client"
+import Earth from '../components/earth/Earth'
+import Link from "next/link";
 
 
 export default function Home() {
-
   return (
     <div className='home'>
       <div className='home__box'>
         <div className='home__title'><span>Move faster </span>with intuitive user interface tools</div>
-        <p className='home__subtitle'>Offers a complete set of user interface tools that will help you implement new features faster. Start with Kit,
+        <p className='home__subtitle'>Offers a complete set of user interface tools that will help you implement new
+          features faster. Start with Kit,
           four fully loaded component libraries, or bring your own design system to our production-ready components.</p>
         <Link href="/kit">
           <button className='home__button'>Get started</button>
         </Link>
+        <Earth/>
       </div>
     </div>
   );
---
+++ components/earth/Earth.jsx
@@ -0,0 +1,53 @@
+import React, { useRef, useEffect } from 'react';
+import * as THREE from 'three';
+import '../earth/earth.css'
+
+const Earth = () => {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+
+    camera.position.z = 5;
+    const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(0x000000, 0);
+
+    const geometry = new THREE.SphereGeometry(2, 32, 32);
+    const texture = new THREE.TextureLoader().load('/planet.jpg');
+    const material = new THREE.MeshBasicMaterial({ map: texture });
+    const sphere = new THREE.Mesh(geometry, material);
+    scene.add(sphere);
+
+    containerRef.current.appendChild(renderer.domElement);
+
+    const frames = 100;
+    const rotationSpeed = 0.01;
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+      sphere.rotation.y += rotationSpeed;
+      renderer.render(scene, camera);
+    };
+    animate();
+
+    return () => {
+      scene.remove(sphere);
+      geometry.dispose();
+      material.dispose();
+      renderer.dispose();
+    };
+  }, []);
+
+  return (
+    <div className='earth' ref={containerRef} />
+  );
+};
+
+export default Earth;
