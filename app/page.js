0d27a5e2842bd4c4930c9aa14b83b41b51dd3caf
Kitsune <87698261+Kat3110@users.noreply.github.com>
2023-06-26 14:24:07 +0600

    add animation home

--- app/page.js
+++ app/page.js
@@ -1,21 +1,29 @@
 "use client"
 import Earth from '../components/earth/Earth'
 import Link from "next/link";
+import Image from "next/image";
+import Sputnik from '../public/sp.png'
+import React from "react";
+// import {useRef, useState, useEffect} from "react";
 
 
 export default function Home() {
   return (
-    <div className='home'>
-      <div className='home__box'>
-        <div className='home__title'><span>Move faster </span>with intuitive user interface tools</div>
-        <p className='home__subtitle'>Offers a complete set of user interface tools that will help you implement new
-          features faster. Start with Kit,
-          four fully loaded component libraries, or bring your own design system to our production-ready components.</p>
-        <Link href="/kit">
-          <button className='home__button'>Get started</button>
-        </Link>
-        <Earth/>
+      <div className='home'>
+        <div className='home__moving'>
+          <Image className='home__img' src={Sputnik}/>
+        </div>
+        <div className='home__box'>
+          <div className='home__title'><span>Move faster </span>with intuitive user interface tools</div>
+          <p className='home__subtitle'>Offers a complete set of user interface tools that will help you implement new
+            features faster. Start with Kit,
+            four fully loaded component libraries, or bring your own design system to our production-ready
+            components.</p>
+          <Link href="/kit">
+            <button className='home__button'>Get started</button>
+          </Link>
+          <Earth/>
+        </div>
       </div>
-    </div>
   );
 }
