dcc683b524d0517158c3225b2c2c28a8ba21ba3c
Kitsune <87698261+Kat3110@users.noreply.github.com>
2023-07-03 14:11:45 +0300

    image optimization

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ export default function RootLayout({children}) {
   return (
     <html lang="en">
       <head>
+        <meta name="description" content="UI-kit"></meta>
         <title>UI Kit test</title>
         <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css"></link>
       </head>
