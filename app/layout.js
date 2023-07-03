331cd61d93e8a711993649286c665e5bc052b9b6
Kitsune <87698261+Kat3110@users.noreply.github.com>
2023-07-03 15:19:13 +0300

    hot fix font

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,21 @@
 import './globals.css'
+import {Encode_Sans_Expanded} from 'next/font/google'
 
+
+const encode = Encode_Sans_Expanded({
+  weight: ['200','300','400','500','600','700','800','900'],
+  subsets: ['latin']
+})
 export default function RootLayout({children}) {
   return (
     <html lang="en">
-      <head>
-        <meta name="description" content="UI-kit"></meta>
-        <title>UI Kit test</title>
-        <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css"></link>
-      </head>
-      <body >
-        {children}
-      </body>
+    <head>
+      <meta name="description" content="UI-kit"></meta>
+      <title>UI Kit test</title>
+      <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css"></link>
+    </head>
+    <body className={encode.className}>
+    {children}
+    </body>
     </html>)
 }
